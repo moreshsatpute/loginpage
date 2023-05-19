381a5cef818e40692c67aa840fbf35dbe46ab82a
Moresh <moreshsatpute@gmail.com>
2023-05-19 22:14:51 +0530

    space add in project

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,8 @@ const Home = () => {
     return (
         <>
             <div className='container mt-3'>
+
+                
                 <section  className='d-flex   justify-content-between' >
                     <div className='left_data mt-3 p-5 m-3' style={{width:"100%"}}>
                         <h3 className='text-center col-lg-6'>Sign Up</h3>
